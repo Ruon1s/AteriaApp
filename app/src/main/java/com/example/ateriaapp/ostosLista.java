ed0bb8a2d7df90549890a48b1b442c54b019cf06
Ruon1s <Mikael.Ruonala@metropolia.fi>
2019-05-03 15:11:16 +0300

    Updatednäkymä

--- app/src/main/java/com/example/ateriaapp/ostosLista.java
+++ app/src/main/java/com/example/ateriaapp/ostosLista.java
@@ -3,6 +3,8 @@ package com.example.ateriaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ostosLista extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ostosLista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ostos_lista);
+
+        populateListView();
     }
+
+    private void populateListView(){
+        String[] myItems = {"blue", "green", "Purple", "Red"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.items, myItems);
+        ListView list = (ListView) findViewById(R.id.ListViewMain);
+        list.setAdapter(adapter);
+    }
+
 }
