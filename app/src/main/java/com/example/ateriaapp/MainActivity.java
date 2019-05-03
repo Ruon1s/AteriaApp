55692e7cbe2f20855441a17133d35000de62740c
Ruon1s <Mikael.Ruonala@metropolia.fi>
2019-05-03 14:07:45 +0300

    Updatednäkymä

--- app/src/main/java/com/example/ateriaapp/MainActivity.java
+++ app/src/main/java/com/example/ateriaapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.ateriaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+
+import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
+    LocalDate date = LocalDate.now();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("aa", "date on" + date);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
