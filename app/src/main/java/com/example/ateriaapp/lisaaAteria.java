ddd553cc77fbb2efa951beec616a42d04246bd34
Ruon1s <Mikael.Ruonala@metropolia.fi>
2019-05-03 12:15:56 +0300

    Erinäkymät

---
+++ app/src/main/java/com/example/ateriaapp/lisaaAteria.java
@@ -0,0 +1,14 @@
+package com.example.ateriaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lisaaAteria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lisaa_ateria);
+    }
+}
---
+++ app/src/main/java/com/example/ateriaapp/ostosLista.java
@@ -0,0 +1,14 @@
+package com.example.ateriaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ostosLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ostos_lista);
+    }
+}
